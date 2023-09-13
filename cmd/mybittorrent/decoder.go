c13ce887d49d67e6bb7f16cba712fe6305c6b142
tonycava <anthony.cavagne@ynov.com>
2023-09-13 17:27:06 +0200

    pass 11st stage

--- cmd/mybittorrent/decoder.go
+++ cmd/mybittorrent/decoder.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/binary"
 	"fmt"
+	"net"
 	"strconv"
 	"unicode"
 )
@@ -97,8 +98,8 @@ func decodePeers(peers []byte) []Peer {
 	var result []Peer
 	for i := 0; i < len(peers); i += 6 {
 		result = append(result, Peer{
-			IP:   fmt.Sprintf("%d.%d.%d.%d", peers[i], peers[i+1], peers[i+2], peers[i+3]),
-			Port: fmt.Sprintf("%d", binary.BigEndian.Uint16(peers[i+4:i+6])),
+			IP:   net.IP(fmt.Sprintf("%d.%d.%d.%d", peers[i], peers[i+1], peers[i+2], peers[i+3])),
+			Port: int(binary.BigEndian.Uint16(peers[i+4 : i+6])),
 		})
 	}
 
