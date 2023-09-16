3f0a32352fc1bf1a5a654d46008b9739a11ae0a2
tonycava <anthony.cavagne@ynov.com>
2023-09-16 15:26:36 +0200

    pass 11st stage

--- cmd/mybittorrent/network.go
+++ cmd/mybittorrent/network.go
@@ -60,11 +60,13 @@ func WaitFor(connection net.Conn, expectedMessageId uint8) []byte {
 		if err != nil {
 			if err == io.EOF {
 				// The remote peer has closed the connection
+				fmt.Println("1111111111111111111111111111111111111111111111")
 				fmt.Println("Connection closed by remote peer.")
 				// You can choose to handle it here or return the error to the caller.
 				// If you want to continue processing, you can decide how to recover.
 			} else {
 				// Some other error occurred
+				fmt.Println("2222222222222222222222222222222222222222222222222")
 				fmt.Println("Error:", err)
 				// Handle the error appropriately
 			}
